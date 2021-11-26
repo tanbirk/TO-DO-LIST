f9ecd85c6583e482c41af59c8708d318feb28eec
tanbirk <tanvir.kashyap879@gmail.com>
2021-11-26 22:15:09 +0530

    To do list

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the task will go */}
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e8eaed',
+   
   },
+  tasksWrapper:{
+      paddingTop: 80,
+      paddingHorizontal: 20,
+  },
+  sectionTitle:{
+
+  },
+  items:{
+    
+  }
 });
